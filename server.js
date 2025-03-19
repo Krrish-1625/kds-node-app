ec4384288d39de1d1b3443819052e53f320ef71f
Krrish-1625 <shindekrushna1525@gmail.com>
2025-03-19 18:48:13 +0000

    Changing the port in the server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // server.js
 const http = require('http');
 
+// Set the port to the value of the environment variable PORT, or default to 8080
+const port = process.env.PORT || 8080;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
-server.listen(3000, 'localhost', () => {
-  console.log('Server running at http://localhost:3000/');
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
